4563142e13f4158eefe3e96dc8cd60e9ac44f7bf
S. Thakur <st670@ew129.eng.cam.ac.uk>
2018-01-30 15:43:43 +0000

    Successful test run

--- test.cpp
+++ test.cpp
@@ -7,10 +7,10 @@ using namespace std;
 #include <robot_delay.h>
 #include <cmath>
 #include "initialise.h"
-#define ROBOT_NUM  15                         // The id number (see below)
+#define ROBOT_NUM  15
 robot_link  rlink;
 int junction_counter = 0;
-int junction_array[5]  = {0,1,1,0,1};
+int junction_array[7]  = {1,0,1,1,0,0,2};
 int array_size = sizeof(junction_array)/sizeof(junction_array[0]);
 
 int BinaryToDecimal(int n) {
@@ -26,15 +26,18 @@ int BinaryToDecimal(int n) {
 
 void timed_forward_motion(int timing) {
     //timing in ms
-    rlink.command (MOTOR_1_GO, 128+38);
-    rlink.command (MOTOR_2_GO, 42);
+    rlink.command (MOTOR_1_GO, 128+68);
+    rlink.command (MOTOR_2_GO, 72);
     delay (timing);
 }
 
 void turn_90_right() {
-    rlink.command (MOTOR_1_GO, 182);
-    rlink.command (MOTOR_2_GO, 182);
-    delay(1000);
+    rlink.command (MOTOR_1_GO, 128+68);
+    rlink.command (MOTOR_2_GO, 72);
+    delay(750);
+    rlink.command (MOTOR_1_GO, 220); 
+    rlink.command (MOTOR_2_GO, 220); 
+    delay(750);
     while (true) {
 		int val = rlink.request (READ_PORT_5);
 		int line_sensors = val & 15; //extract 4 most LSB values 
@@ -47,10 +50,13 @@ void turn_90_right() {
 
 void junction_tester() {
     if (junction_array[junction_counter]==0) {
-        timed_forward_motion(1000);
+        timed_forward_motion(500);
     }
     if (junction_array[junction_counter]==1) {
         turn_90_right();
+    }
+    if (junction_array[junction_counter]==2) {
+        timed_forward_motion(4500);
     }
     cout<<junction_counter<<endl;
     junction_counter++;
@@ -71,6 +77,7 @@ int main() {
             cout<<"BREAKING WITH:"<<line_sensors<<endl;
             break;
         }
+         
 		
         switch (line_sensors) {
             case 6 : //0110
@@ -98,11 +105,12 @@ int main() {
                 rlink.command (MOTOR_1_GO, 10);
                 rlink.command (MOTOR_2_GO, 72+55);
                 break;
+                
             case 0 : //0000
                 cout<<"0000 DANGER: Off path"<<endl;
                 rlink.command (MOTOR_1_GO, 20);
                 rlink.command (MOTOR_2_GO, 128+20);
-                break;
+                break; 
 
         }
         
